3013afb69348f565250570cb0b0dccffe12d8f1a
djtcarrilho <djtcarrilho@hotmail.com>
2016-05-07 13:03:41 +0100

    added unfinished user control to change language and place master page controls inside form tag

---
+++ MultiLangPrj/Controls/ChooseLanguage.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ChooseLanguage.ascx.cs" Inherits="MultiLangPrj.Controls.ChooseLanguage" %>
+
+
+<%-- <asp:ImageButton ID="ImageButton1_EN" CommandArgument="fr" runat="server" OnClick="ImageButton1_Click" />
+    <asp:ImageButton ID="ImageButton2_PT" runat="server" OnClick="ImageButton2_Click" CommandArgument="pt"/>--%>
--- MultiLangPrj/Site1.Master.designer.cs
+++ MultiLangPrj/Site1.Master.designer.cs
@@ -49,6 +49,24 @@ namespace MultiLangPrj {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
         
         /// <summary>
+        /// ImageButton1_EN control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1_EN;
+        
+        /// <summary>
+        /// ImageButton2_PT control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton2_PT;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
@@ -56,23 +74,5 @@ namespace MultiLangPrj {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-        
-        /// <summary>
-        /// ImageButton1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
-        
-        /// <summary>
-        /// ImageButton2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ImageButton2;
     }
 }
