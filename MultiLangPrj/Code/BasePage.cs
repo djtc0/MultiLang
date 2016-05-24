af4d76d2d52a9e6a307424ed319b409febd0ab7d
djtcarrilho <djtcarrilho@hotmail.com>
2016-05-24 22:01:43 +0100

    still cookies but language set in global.asax

--- MultiLangPrj/Code/BasePage.cs
+++ MultiLangPrj/Code/BasePage.cs
@@ -11,35 +11,6 @@ namespace MultiLangPrj.Code
     {
         protected override void InitializeCulture()
         {
-            string language = "pt";
-
-            //Detect User's Language.
-            if (Request.UserLanguages != null)
-            {
-                //Set the Language.
-                language = Request.UserLanguages[0];
-            }
-            string lang = string.Empty;//default to the invariant culture
-
-            
-
-            if (Request.Form["ctl00$HF_CurrentSelectedLanguage"] != null)
-            {
-                language = Request.Form["ctl00$HF_CurrentSelectedLanguage"];
-            }
-
-            else //look at the cookie
-            {
-                HttpCookie cookie = Request.Cookies["Language"];
-                if (cookie != null)
-                {
-                    language = cookie.Value;
-                }
-            }
-            
-            //Set the Culture.
-            Thread.CurrentThread.CurrentCulture = new CultureInfo(language);
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
         }
     }
 }
